37d4e44e1d441badc97b3886cf51f65319c71ce6
wansom <ochiengwarren10@gmail.com>
2023-11-15 04:28:41 +0300

    research papers

--- src/views/dashboard/ResearchPapers.vue
+++ src/views/dashboard/ResearchPapers.vue
@@ -29,56 +29,153 @@
                             </div>
                             <div class="cards">
                                 <div class="card-topic">
-                                    <h2><i class="fa-solid fa-arrow-trend-up"></i> Trending Challenges</h2>
+                                    <h2><i class="fa-solid fa-arrow-trend-up"></i> Trending Research Papers</h2>
                                     <a href="#">View All</a>
                                 </div>
-                                <div class="topic-cards grid grid-cols-1 md:grid-cols-3">
-                                    <div class="topic-card">
-                                        <div class="img">
-                                            <img src="/images/topic image.jpg" alt="afroaiinnovate">
-                                        </div>
-                                        <div class="topic-card-info">
-                                            <a href="single_challenge.html">Movies & Ratings for Recommendation System</a>
-                                            <p>Nicoleta Cilibiu · Updated 3 days ago</p>
-                                            <p>Usability <strong>9.4</strong> · 866 kB</p>
-                                            <p>2 Files (CSV)</p>
-                                        </div>
-                                        <div class="pricing">
-                                            <span>$100,000</span>
-                                            <span>2 months to go</span>
-                                        </div>
-                                    </div>
-                                    <div class="topic-card">
-                                        <div class="img">
-                                            <img src="/images/topic image.jpg" alt="afroaiinnovate">
-                                        </div>
-                                        <div class="topic-card-info">
-                                            <a href="single_challenge.html">Movies & Ratings for Recommendation System</a>
-                                            <p>Nicoleta Cilibiu · Updated 3 days ago</p>
-                                            <p>Usability <strong>9.4</strong> · 866 kB</p>
-                                            <p>2 Files (CSV)</p>
-                                        </div>
-                                        <div class="pricing">
-                                            <span>$100,000</span>
-                                            <span>2 months to go</span>
-                                        </div>
-                                    </div>
-                                    <div class="topic-card">
-                                        <div class="img">
-                                            <img src="/images/topic image.jpg" alt="afroaiinnovate">
-                                        </div>
-                                        <div class="topic-card-info">
-                                            <a href="single_challenge.html">Movies & Ratings for Recommendation System</a>
-                                            <p>Nicoleta Cilibiu · Updated 3 days ago</p>
-                                            <p>Usability <strong>9.4</strong> · 866 kB</p>
-                                            <p>2 Files (CSV)</p>
-                                        </div>
-                                        <div class="pricing">
-                                            <span>$100,000</span>
-                                            <span>2 months to go</span>
-                                        </div>
-                                    </div>
-                                </div>
+                                <div class="listed-list">
+     
+            <div class="listed-list-lists">
+
+              <div class="list-card">
+                <div class="card-left">
+                  <div class="card-image">
+                    <img src="/images/paper.png" alt="hackathon" class="h-[200px] w-[250px]">
+                  </div>
+                  <div class="card-info ">
+                    <div class="title">
+                      <h2>Zephyr: Direct Distillation of LM Alignment</h2>
+                      <p class="paper-desc">Starting from a dataset of outputs ranked by a teacher model, we apply distilled direct preference optimization (dDPO) to learn a chat model with significantly improved intent alignment.</p>
+                    </div>
+                    <div class="card-info-extra">
+                      <div class="bottom">
+                        <div class="bottom-left flex items-center gap-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+</svg>
+
+                          <span><strong class="mr-1">5,500</strong>Github Stars</span>
+                        </div>
+                        <div class="bottom-right flex items-center gap-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+</svg>
+
+                          <span><strong class="mr-1">20</strong> Downloads</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-right">
+                  <div class="layers flex items-center gap-4">
+                    <div class="left-layer">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+</svg>
+
+                    </div>
+                    <div class="right-layer">
+                      <span class="date">
+                        huggingface
+                      </span>
+                    </div>
+                  </div>
+                  <div class=" flex items-center gap-4">
+                    <div class="left-layer">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+</svg>
+
+                    </div>
+                    <div class="right-layer">
+                      <span class="date">
+                         Dec 04, 2023
+                      </span>
+                    </div>
+                  </div>
+                  <div class="layers">
+                    <div class="left-layer">
+                      <i class="fa-solid fa-tag"></i>
+                    </div>
+                    <div class="right-layer">
+                      <span class="tags">View All</span>
+                      <span class="tags">Get Code</span>
+                      <span class="tags whitespace-nowrap">Download Paper</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="list-card">
+                <div class="card-left">
+                  <div class="card-image">
+                    <img src="/images/paper.png" alt="hackathon" class="h-[200px] w-[250px]">
+                  </div>
+                  <div class="card-info ">
+                    <div class="title">
+                      <h2>Zephyr: Direct Distillation of LM Alignment</h2>
+                      <p class="paper-desc">Starting from a dataset of outputs ranked by a teacher model, we apply distilled direct preference optimization (dDPO) to learn a chat model with significantly improved intent alignment.</p>
+                    </div>
+                    <div class="card-info-extra">
+                      <div class="bottom">
+                        <div class="bottom-left flex items-center gap-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+</svg>
+
+                          <span><strong class="mr-1">5,500</strong>Github Stars</span>
+                        </div>
+                        <div class="bottom-right flex items-center gap-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+</svg>
+
+                          <span><strong class="mr-1">20</strong> Downloads</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-right">
+                  <div class="layers flex items-center gap-4">
+                    <div class="left-layer">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+</svg>
+
+                    </div>
+                    <div class="right-layer">
+                      <span class="date">
+                        huggingface
+                      </span>
+                    </div>
+                  </div>
+                  <div class=" flex items-center gap-4">
+                    <div class="left-layer">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+</svg>
+
+                    </div>
+                    <div class="right-layer">
+                      <span class="date">
+                         Dec 04, 2023
+                      </span>
+                    </div>
+                  </div>
+                  <div class="layers">
+                    <div class="left-layer">
+                      <i class="fa-solid fa-tag"></i>
+                    </div>
+                    <div class="right-layer">
+                      <span class="tags">View All</span>
+                      <span class="tags">Get Code</span>
+                      <span class="tags whitespace-nowrap">Download Paper</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
                             </div>
                         </div>
                     </div>
